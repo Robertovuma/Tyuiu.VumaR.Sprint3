ac9f99c0c86dcf8853a3feae3d716603dac84ae6
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-26 01:51:40 +0500

    Add project files.

---
+++ Tyuiu.VumaR.Sprint3.Task0.V4.Test/Test1.cs
@@ -0,0 +1,21 @@
+﻿
+using Tyuiu.VumaR.Sprint3.Task0.V4.Lib;
+namespace Tyuiu.VumaR.Sprint3.Task0.V4.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 7;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+            double wait = 28.699;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VumaR.Sprint3.Task0.V4/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Tyuiu.VumaR.Sprint3.Task0.V4.Lib;
+
+
+namespace Tyuiu.VumaR.Sprint3.Task0.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 1;
+            int stopValue = 7;
+
+            Console.WriteLine("Начало шага=" + startValue);
+            Console.WriteLine("Конец шага=" + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда=" + ds.GetMultiplySeries(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
